77f38545eaeffe124c2d79b85e97bdf019aca4bb
Nick Howard <ndh@baroquebobcat.com>
2013-08-06 22:04:13 -0600

    added helper for type check

--- lib/muskox.rb
+++ lib/muskox.rb
@@ -29,11 +29,9 @@ module Muskox
           case stack.last.first
           when :property
             last = stack.last
-            if expected_type(schema_stack.last, last) == "array"
+            matching_type expected_type(schema_stack.last, last), "array" do
               stack.push [:array, []]
               schema_stack.push(schema["properties"][last.last])
-            else
-              raise ParserError
             end
           end
         when :array_end
@@ -42,10 +40,8 @@ module Muskox
           case stack.last.first
           when :property
             last = stack.pop
-            if expected_type(schema_stack.last, last) == "array"
+            matching_type expected_type(schema_stack.last, last), "array" do
               stack.last.last[last.last] = array_top.last
-            else
-              raise ParserError
             end
           else
             raise "unknown stack type #{stack.last.first}"
@@ -54,47 +50,37 @@ module Muskox
           case stack.last.first
           when :property
             last = stack.last
-            if expected_type(schema_stack.last, last) == "object"
+            matching_type expected_type(schema_stack.last, last), "object" do
               stack.push [:object, {}]
               schema_stack.push(schema["properties"][last.last])
-            else
-              raise ParserError
             end
           when ROOT
             stack.push [:object, {}]
           end
         when :object_end
           object_top = stack.pop
-          
           case stack.last.first
           when :property
             schema_stack.pop
             last = stack.pop
-            if expected_type(schema_stack.last, last) == "object" && stack.last.first == :object
+            matching_type expected_type(schema_stack.last, last), "object", stack.last.first == :object do
               stack.last.last[last.last] = object_top.last
-            else
-              raise ParserError
             end
           when ROOT
             r = object_top.last
           else
-            raise "unknown stack type #{stack.last && stack.last.first}"
+            raise "unknown stack type #{stack.last.first}"
           end
-
         when :integer, :string
           case stack.last.first
           when :property
             last = stack.pop
-            if expected_type(schema_stack.last, last) == type.to_s && stack.last.first == :object
+            matching_type expected_type(schema_stack.last, last), type, stack.last.first == :object do
               stack.last.last[last.last] = value
-            else
-              raise ParserError
             end
           when :array
-            if schema_stack.last["items"]["type"] == type.to_s
+            matching_type schema_stack.last["items"]["type"], type do
               stack.last.last << value
-            else
-              raise ParserError
             end
           else
             raise "unknown stack type #{stack.last.first}"
@@ -106,6 +92,14 @@ module Muskox
 
     def expected_type schema, last
       schema["properties"][last.last] && schema["properties"][last.last]["type"]
+    end
+
+    def matching_type expected, actual, opt=true
+      if expected == actual.to_s && opt
+        yield
+      else
+        raise ParserError
+      end
     end
   end
 
